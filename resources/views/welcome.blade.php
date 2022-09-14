135bf8b4b81222d62cd029d5170c5aa38d4ce801
Maciek Palmowski <m.palmowski@freshpixels.pl>
2022-09-14 10:47:59 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
